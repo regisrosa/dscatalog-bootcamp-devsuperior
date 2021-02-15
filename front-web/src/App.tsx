9c5dedcdb7f49dadaf990d9e43a895234e891f40
Regis Rosa <regisaraujorosa@gmail.com>
2021-02-15 09:37:51 -0300

    Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './core/assets/styles/custom.scss';
 import './app.scss';
-import Navbar from './core/assets/styles/components/Navbar';
-
+import Routes from './Routes';
 
 
 
 const App = () => {
     
     return (
-        <Navbar />
+       <Routes/>
     );
 }
 
---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Navbar from './core/assets/styles/components/Navbar';
+import Home from './pages/Home';
+import Admin from './pages/Home/Admin';
+import Catalog from './pages/Home/Catalog';
+
+
+
+const Routes = () => (
+    <BrowserRouter>
+        <Navbar/>
+        <Switch>
+            <Route path="/" exact>
+                <Home/>
+            </Route>
+            <Route path="/catalog">
+                <Catalog/>
+            </Route>
+            <Route path="/admin">
+                <Admin/>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
